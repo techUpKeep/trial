164310e9f50b1f4a2b31735669a16144644320ea
Tech Upkeep <83671738+techUpKeep@users.noreply.github.com>
2022-08-11 14:30:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,13 +82,14 @@ addtotrolleybtnDom.forEach(addtotrolleybtnDom => {
       <input type="hidden" name="_captcha" value="false">
       <input type="hidden" name="_next" value="https://techupkeep.github.io/trial/thankyou.html">
   
+<input type="hidden" name="_subject" value=${totalY}>
+
 
 
 
                   <label for="totalY" id="totalY">abcd</label>
 
-                  <label>abcd</label>
-                  <label for="twenty">14.20</label>
+                  <label for="twenty">14.30</label>
 
 
 
